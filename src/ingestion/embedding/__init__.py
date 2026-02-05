804121bad0a17112a3803b6621eb4bd5c1be5cfc
lzp-mayun <liuzepeng0200@163.com>
2026-02-05 08:37:41 +0800

    feat(ingestion): add BatchProcessor (C10)

--- src/ingestion/embedding/__init__.py
+++ src/ingestion/embedding/__init__.py
@@ -1,4 +1,5 @@
 from src.ingestion.embedding.dense_encoder import DenseEncoder
 from src.ingestion.embedding.sparse_encoder import SparseEncoder
+from src.ingestion.embedding.batch_processor import BatchProcessor, BatchMetrics, BatchProcessResult
 
-__all__ = ["DenseEncoder", "SparseEncoder"]
+__all__ = ["DenseEncoder", "SparseEncoder", "BatchProcessor", "BatchMetrics", "BatchProcessResult"]
